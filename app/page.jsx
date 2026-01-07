db7218a28e7c6af07390756fb59989639c8c10cc
ArnP95 <ArniP.tmi@outlook.com>
2026-01-07 17:43:34 +0200

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+ap-arktech-site/       ← pääkansio (root)
+│
+├─ app/                ← tämä kansio on pakollinen
+│   └─ page.jsx        ← tänne sijoitat koodin, jonka lähetit minulle
+│
+├─ public/
+│   └─ images/...      ← kuvasi
+├─ package.json
+└─ README.md
+Lisätty page.jsx pääsivuksi
